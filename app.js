b016e54934957bc7ecf46ab0896265f4e6e06588
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-07 09:56:39 -0300

    initial structure

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+
+mongoose
+  .connect('mongodb://localhost/xxTech', {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+
+// Express View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// default value for title local
+app.locals.title = 'XX Tech';
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+module.exports = app;
