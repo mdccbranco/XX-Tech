92a91eefd5316e84ccc10e3ef5a32147048e3672
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-08 16:18:35 -0300

    category of post

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,22 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/yes-she-can', (req, res, next) => {
-  Commit.find({post: true})
+  Commit.find({post: true, category: 'yes'})  
     .populate('owner')
     .then(commit => {
-      res.render('home/yes', {commit});
+      // res.send(commit);
+     res.render('home/yes', {commit});
+    })
+    .catch(error => console.log(error));
+});
+
+router.get('/inspiration-bits', (req, res, next) => {
+  Commit.find({post: true, category: 'bits'})
+  // Commit.find()
+    .populate('owner')
+    .then(commit => {
+      // console.log(commit);
+      res.render('home/bits', {commit});
     })
     .catch(error => console.log(error));
 });
