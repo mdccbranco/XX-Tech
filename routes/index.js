feaacb368cceec502978ca7d844b603fc5b13db2
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-10 15:40:54 -0300

    Search on views

--- routes/index.js
+++ routes/index.js
@@ -77,6 +77,40 @@ router.get('/yes-she-can', (req, res, next) => {
     .catch(error => console.log(error));
 });
 
+router.post('/yes-she-can/search', (req, res, next) => {
+  let isAdm = false;
+  let isUser = false;
+  let search = req.body.search;
+  if (req.user !== undefined) {
+    if (req.user.role === 'adm') {
+      isAdm = true;
+    } else if (req.user.role === 'user') {
+      isUser = true;
+    }
+  }
+  Commit.find({
+      post: true,
+      category: 'yes', $text:
+        {
+          $search: search,
+          $caseSensitive: false
+        }
+    })
+    .populate('owner')
+    .then(commit => {
+      console.log(commit);
+      // res.send(commit);
+      res.render('home/yes', {
+        commit,
+        user: req.user,
+        isAdm,
+        isUser,
+        ola
+      });
+    })
+    .catch(error => console.log(error));
+});
+
 router.get('/inspiration-bits', (req, res, next) => {
   let isAdm = false;
   let isUser = false;
@@ -94,6 +128,38 @@ router.get('/inspiration-bits', (req, res, next) => {
     .populate('owner')
     .then(commit => {
       // res.send(commit);
+      res.render('home/bits', {
+        commit,
+        user: req.user,
+        isAdm,
+        isUser,
+        ola
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+router.post('/inspiration-bits/search', (req, res, next) => {
+  let isAdm = false;
+  let isUser = false;
+  let search = req.body.search;
+  if (req.user !== undefined) {
+    if (req.user.role === 'adm') {
+      isAdm = true;
+    } else if (req.user.role === 'user') {
+      isUser = true;
+    }
+  }
+  Commit.find({
+      post: true,
+      category: 'bits', $text:
+      {
+        $search: search,
+        $caseSensitive: false
+      }
+    })
+    .populate('owner')
+    .then(commit => {
       res.render('home/bits', {
         commit,
         user: req.user,
