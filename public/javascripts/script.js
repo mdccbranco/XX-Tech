535fb4adea8a2b26489f3647559fac647f185328
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-10 23:03:55 -0300

    unnecessary logs removed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('click', function(e) {
-  console.log(e.target);
   if (e.target && e.target.name === 'category') {
     if (e.target.value === 'yes' || e.target.value === 'bits') {
       document.getElementById('link-info').hidden = !0;
